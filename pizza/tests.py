92babe9995248b5cd1fc1949fe695df8a82c0715
Керов Евгений Сергеевич <gr486_kes@ttit.local>
2021-05-14 13:52:57 +0700

    gk

--- pizza/tests.py
+++ pizza/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import *
+from faker import Faker
 
-# Create your tests here.
+fake = Faker()
+
+
+class ProductsTest(TestCase):
+    category = fake.name()
+    name = fake.name()
+    description = fake.text()
+    price = fake.pydecimal()
+    image = fake.file_name('image')
+    slug = fake.slug()
+
+    def setUp(self):
+        Products.objects.create(category=self.category, name=self.name, description=self.description,
+                                price=123,
+                                image=self.image, slug=self.slug)
+
+    def test_product_equal(self):
+        prod = Products.objects.get(category=self.category)
+        self.assertEqual(prod.category, self.category)
