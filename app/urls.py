5aeff594431f3f932c445644ce000f89c7c7a5ac
kerosin1337 <62531569+kerosin1337@users.noreply.github.com>
2021-05-24 01:07:35 +0700

    gj

--- app/urls.py
+++ app/urls.py
@@ -35,12 +35,14 @@ urlpatterns = [
     path('staff/', views.room, name='staff')
 ]
 
-if settings.DEBUG:
-    # urlpatterns += [
-    #     url(r'^media/(?P<path>.*)$', serve, {
-    #         'document_root': settings.MEDIA_ROOT,
-    #     }),
-    # ]
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     # urlpatterns += [
+#     #     url(r'^media/(?P<path>.*)$', serve, {
+#     #         'document_root': settings.MEDIA_ROOT,
+#     #     }),
+#     # ]
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += router.urls
